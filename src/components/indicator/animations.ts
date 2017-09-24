4a6970fa9ebf61f3bc3ca63f6844f35cdf884cde
Robbert van Markus <robbert@vanmarkus.com>
2017-09-24 22:27:23 +0200

    error animation

---
+++ src/components/indicator/animations.ts
@@ -0,0 +1,12 @@
+import { keyframes } from 'styled-components';
+export const errorAnimation = keyframes`
+0%{
+	background:rgb(0,0,0);
+  }
+  11%{
+	background: rgba(0,0,0);
+  }
+  100%{
+	background: rgb(0,187,255);
+  }
+`;
--- src/components/indicator/index.tsx
+++ src/components/indicator/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { keyframes } from 'styled-components';
 import * as classNames from 'classnames';
 
-export type IndicateTypes = 'error' | 'warningn' | 'loading';
+export type IndicateTypes = 'error' | 'warningn' | 'loading' | boolean;
 
 export interface InidcatorProps {
-  indicate: boolean | IndicateTypes;
+  indicate: IndicateTypes;
   className?: string;
 }
 
@@ -16,6 +17,27 @@ const Indicator: React.StatelessComponent<InidcatorProps> = props => (
       })}
   > &nbsp; 
   </span>);
+
+export const errorAnimation = keyframes`
+0%{
+	background:rgb(0,0,0);
+  }
+  11%{
+	background: rgba(0,0,0);
+  }
+  100%{
+	background: rgb(0,187,255);
+  }
+`;
+
+function getAnimationForIndication(inidcation: IndicateTypes): string {
+  switch (inidcation) {
+      case 'error':
+        return errorAnimation;
+      default: 
+      return '';    
+  }
+}
 
 const SwitchIndicator = styled(Indicator)`
   content: "";
@@ -34,6 +56,7 @@ const SwitchIndicator = styled(Indicator)`
   inset 0 -5px 5px rgba(0,0,0,0.5);
   -webkit-transition: all .5s ease;
   transition: all .5s ease;
+  animation: ${props => getAnimationForIndication(props.indicate)} 0.9s ease-out 0s infinite alternate;
   z-index: 2;
   &.indicate {
     content: "";
@@ -45,7 +68,7 @@ const SwitchIndicator = styled(Indicator)`
     -webkit-transition: all .5s ease;
     transition: all .5s ease;
     z-index: 2;
-    background: ${props => props.indicate === 'error' ? 'red' : '#b9f3fe'}; 
+    background: ${props => props.indicate === 'error' ? '' : '#b9f3fe'}; 
     background: gradient-gradient(#ffffff, #77a1b9);
     box-shadow:        
         inset 0 1px 0 rgba(0,0,0,0.1),
