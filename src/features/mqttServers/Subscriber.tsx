f76bdad5c27c6a74fcf74f4c7572198e20f1a6c0
robbertvanmarkus <rvanmarkus@xebia.com>
2017-09-26 22:40:53 +0200

    wip subscriber

---
+++ src/features/mqttServers/Subscriber.tsx
@@ -0,0 +1,15 @@
+import { MqttClient } from 'mqtt/types';
+import * as react from 'react';
+
+interface SubscriberProps {
+  client?: MqttClient;
+}
+interface SubscriberState {
+
+}
+export class Subscriber extends react.Component<SubscriberProps, SubscriberState> {
+
+  render() {
+    return ();
+  }
+}
