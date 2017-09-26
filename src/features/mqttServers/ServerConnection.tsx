07d137ef1b399b1d7b16e493f27afc4d4b13270d
robbertvanmarkus <rvanmarkus@xebia.com>
2017-09-26 17:45:57 +0200

    working mqtt connection

--- src/features/mqttServers/ServerConnection.tsx
+++ src/features/mqttServers/ServerConnection.tsx
@@ -5,6 +5,7 @@ import { EditServerConfigButton } from './EditServerAddressDialog';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { AddServerConfig, addServerConfig } from '../../actions/index';
+import { MqttClient, connect as connectClient } from 'mqtt';
 
 interface ServerConnectionProps {
     server: MQTTServerConfig;
@@ -13,6 +14,7 @@ interface ServerConnectionProps {
 
 interface ServerListState {
     editServerAddressDialogOpen: boolean;
+    connected: boolean;
 }
 
 function mapStateToProps({serverConfig}: RootState): ServerConnectionProps {
@@ -28,9 +30,35 @@ function mapDispatchToProps(dispatch: Dispatch<AddServerConfig>): Partial<Server
 }
 
 class ServerConnectionIndicator extends React.Component<ServerConnectionProps, ServerListState> {
+    client: MqttClient;
     state = {
         editServerAddressDialogOpen: false,
+        connected: false
     };
+    
+    constructor() {
+        super();    
+    }
+    componentWillMount() {
+        const {server} = this.props;
+
+        if (server) {
+            this.connectMqttClient(server.address);
+        }
+    }
+    onClientConnect() {
+        this.setState({connected: true});
+    }
+    connectMqttClient(serverAddress: string) {
+        this.client = connectClient(serverAddress);
+        this.client.on('connect', () => this.onClientConnect());
+        this.client.on('close', () => this.onDisconnect());
+    }
+    onDisconnect() {
+        this.setState({
+            connected: false
+        });
+    }
     render() {
         return (
             <Menu>
@@ -41,17 +69,26 @@ class ServerConnectionIndicator extends React.Component<ServerConnectionProps, S
 
     }
     currentServerName() {
-
         if (this.props.server) {
-            return <MenuItem text={this.props.server.name} key={this.props.server.name} />;
+            return (
+            <MenuItem 
+                text={this.postFixServerNameWhenOnline(this.props.server.name)} 
+                key={this.props.server.name} 
+            />);
         } 
-    
+
         return <MenuItem text="No server configured yet" key="empty" />;
+    }
+
+    postFixServerNameWhenOnline(serverName: string) {
+        return `${serverName} ${this.state.connected ? '(Online)' : '(Offline)'}`;
     }
     onServerAdded(server: MQTTServerConfig) {
         if (this.props.onAddServer) {
             this.props.onAddServer(server);
         }
+        this.setState({connected: false});
+        this.connectMqttClient(server.address);
     }
 }
 
--- src/features/mqttServers/config.ts
+++ src/features/mqttServers/config.ts
@@ -2,5 +2,5 @@ import { MQTTServerConfig } from '../../types/index';
 
 export const defaultServerConfig: MQTTServerConfig = {
     name: 'Default MQTT Server (Localhost)',
-    address: 'ws://localhost:4200'
+    address: 'ws://localhost:1884'
 };
